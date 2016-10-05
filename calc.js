388e957e521f10c0b4821babf3d1b98024ed1c59
Simon Casey <simon.jk.casey@gmail.com>
2016-10-05 13:25:31 +1300

    add operator input, calcfns

--- calc.js
+++ calc.js
@@ -17,18 +17,24 @@ $(document).ready(function(){
     number += $(this).val();
     totalDisplay.text(number);
     overflowCheck(number);
-  })
-  $(".operator").click(function){
+  });
+  $(".operator").click(function(){
     operator = $(this).val();
     newNumber = number;
     number = "";
     totalDisplay.text("0");
-  }
-  $(".calc_fn").click(function){
+  });
+  $(".calc_fn").click(function(){
     if ($(this).attr("name") === "clr") {
       number = "";
       totalDisplay.text("0")
     }
-  }
+    if ($(this).attr("name") === "clr-all") {
+      number = "";
+      newNumber = "";
+      operator = "";
+      totalDisplay.text(0);
+    }
+  });
 
 });
