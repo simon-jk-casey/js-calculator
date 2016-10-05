6a769c3d2a1a6422f2b9392128d3962f59252b85
Simon Casey <simon.jk.casey@gmail.com>
2016-10-05 13:39:44 +1300

    add calculation functions

--- calc.js
+++ calc.js
@@ -11,19 +11,22 @@ $(document).ready(function(){
   var newNumber = "";
   var operator = "";
   var totalDisplay = $("#display");
+  //var memory = "";
   totalDisplay.text("0");
-
+  //number input
   $(".num").click(function(){
     number += $(this).val();
     totalDisplay.text(number);
     overflowCheck(number);
   });
+  //operator input
   $(".operator").click(function(){
     operator = $(this).val();
     newNumber = number;
     number = "";
     totalDisplay.text("0");
   });
+  //calculator function buttons
   $(".calc_fn").click(function(){
     if ($(this).attr("name") === "clr") {
       number = "";
@@ -35,6 +38,25 @@ $(document).ready(function(){
       operator = "";
       totalDisplay.text(0);
     }
+    //MEM-ADD and MEM-RECALL HERE
   });
+  //calculations
+  $(".equals").click(function(){
+    switch(operator) {
+      case '+':
+        number = (parseInt(newNumber, 10) + parseInt(number, 10)).toString(10);
+        break;
+      case '-':
+        number = (parseInt(newNumber, 10) - parseInt(number, 10)).toString(10);
+        break;
+      case '*':
+        number = (parseInt(newNumber, 10) * parseInt(number, 10)).toString(10);
+        break;
+      case '/':
+        number = (parseInt(newNumber, 10) / parseInt(number, 10)).toString(10);
+    }
+    totalDisplay.text(number);
+    overflowCheck(number);
+  })
 
 });
