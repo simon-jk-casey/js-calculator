6583f16b6c1acc4de772d51e49d1b2f9ff1c233a
Simon Casey <simon.jk.casey@gmail.com>
2016-10-05 13:07:00 +1300

    add num input fn

---
+++ calc.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+  function overflowCheck(number) {
+    if (number.length > 12) {
+      number = "";
+      totalDisplay.text("Error");
+    };
+  };
+
+  var number = "";
+  var newNumber = "";
+  var operator = "";
+  var totalDisplay = $("#display");
+  totalDisplay.text("0");
+
+  $(".num").click(function(){
+    number += $(this).val();
+    totalDisplay.text(number);
+    overflowCheck(number);
+  })
+  $(".operator").click(function){
+    operator = $(this).val();
+    newNumber = number;
+    number = "";
+    totalDisplay.text("0");
+  }
+  $(".calc_fn").click(function){
+    if ($(this).attr("name") === "clr") {
+      number = "";
+      totalDisplay.text("0")
+    }
+  }
+
+});
